58b40f403f2f2ba6ab663234bf204a22a0c3b875
rm.amaral <rm.amaral@campus.fct.unl.pt>
2018-06-14 23:58:46 +0100

    olfasd

--- handout4/RainWindProtection.java
+++ handout4/RainWindProtection.java
@@ -1,7 +1,17 @@
 /*@
 
-	predicate RainWindProtectionInv ( Actuator a; ) = 
-			a.value |-> ?v;
+	predicate IndoorLightsInv ( IndoorLights il; ) = 
+			il.value |-> ?v
+			&*& il.sensorList |-> ?s1
+			&*& il.windowsList |-> ?w2
+			&*& il.nSensors |-> ?ns
+			&*& il.nWindows |-> ?nw
+			&*& 0 <= ns &*& ns <= s1.length
+			&*& 0 <= nw &*& nw <= w2.length
+			&*& array_slice(sensorList,0,ns,?elems)
+			&*& array_slice(sensorList,ns,s1.length,?others)
+			&*& array_slice(windowsList,0,nw,?elems)
+			&*& array_slice(windowsList,nw,w2.length,?others);
 @*/
 
 class RainWindProtection {
@@ -25,7 +35,7 @@ class RainWindProtection {
         
         	while(true){
             
-            		TimeUnit.SECONDS.sleep(5);
+            		
                 	Sensor rain = sensorList.get(0);
                 	Sensor wind = sensorList.get(1);
                 
@@ -36,11 +46,12 @@ class RainWindProtection {
 	
                 	if(val1 >  5 && val2 > 10){
                         
-                        	//for(Actuator a : windowsList){
-                        	a.set(1);                        
-                        	//}
+                        	for(int i = 0; i < windowsList.length; i++){
+                        		windowsList[i].set(true);                        
+                        	}
 
                         	log.write("Rain/WindProtection", output, "true");
+                        	TimeUnit.SECONDS.sleep(5);
                 	}
     		}   
     	}	
